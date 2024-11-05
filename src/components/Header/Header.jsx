c11728fdf428c39924ab061cfa97ef1a1107cad0
Istiak <mhistiak2@gmail.com>
2024-11-05 21:38:00 +0600

    make header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
+import { FaBars, FaTimes } from "react-icons/fa";
 import classes from "./Header.module.css";
+import { useRef } from "react";
 const Header = () => {
+  const menuRef = useRef(null);
+  const showMenu = () => {
+    menuRef.current.classList.add(classes.active_menu);
+
+  };
+  const hideMenu = () => {
+    menuRef.current.classList.remove(classes.active_menu);
+  };
   return (
     <header className={classes.header_section}>
       <nav className={classes.nav}>
         <div className={`${classes.nav_container} container`}>
           <img src="/logo.png" alt="logo" className={classes.logo} />
-          <menu className={classes.menu_container}>
+          <menu className={classes.menu_container} ref={menuRef}>
+            <span className={classes.hide_icon} onClick={hideMenu}>
+              <FaTimes />
+            </span>
             <div className={classes.menu_items}>
               <a href="#">Home</a>
               <a href="#">About</a>
@@ -18,6 +31,9 @@ const Header = () => {
               Book A Table
             </a>
           </menu>
+          <span className={classes.hamberger_icon} onClick={showMenu}>
+            <FaBars />
+          </span>
         </div>
       </nav>
       <div className={classes.hero_section}>
